bab11c766f6b04a5a2ba67c70c01bfe3b43c0de1
ishmamk1 <ishmamkhan1010@gmail.com>
2025-02-22 11:08:39 -0500

    updated navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import AppProvider from "./store/appProvider.tsx"; // Assuming AppProvider is the context provider
 import Router from './Router.tsx';
 import Navbar from './components/Navbar.tsx';
-import VideoComparison from './components/VideoComparison.tsx';
 
 
 
@@ -10,7 +9,6 @@ const App: React.FC = () => {
   return (
     <AppProvider>
       <Navbar/>
-      <VideoComparison/>
       <Router/>
     </AppProvider>
   );
