535ef5c5742b20429ce5ef24567611afaea2dee9
Sudiptto Biswas <biswassudiptto@gmail.com>
2025-02-22 01:27:43 -0500

    pushed first three pages

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import LandingPage from "./pages/LandingPage";
+import HomePage from "./pages/HomePage";
+import SelectDrill from "./pages/selectDrill";
+import UploadDrill from "./pages/UploadDrill";
 import NotFound from "./pages/NotFound";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -12,10 +15,14 @@ const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" Component={LandingPage}></Route>
-        <Route path="*" Component={NotFound}></Route>
+
+        <Route path="/" Component={HomePage}></Route>
+        <Route path="/selectDrill" Component={SelectDrill}></Route>
+        <Route path="/uploadDrill" Component={UploadDrill}></Route>
+        
+        {/* <Route path="*" Component={NotFound}></Route>
         <Route path="/login" Component={LoginPage}></Route>
-        <Route path="/register" Component={RegisterPage}></Route>
+        <Route path="/register" Component={RegisterPage}></Route> */}
       </Routes>
     </BrowserRouter>
   );
